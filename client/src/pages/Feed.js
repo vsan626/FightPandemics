9ea6f291c14688da081e35c661523ee18151f5a7
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-08-07 03:11:39 +0300

    fix: infinite loader implementation improvements

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -223,7 +223,9 @@ const Feed = (props) => {
   const [selectedOptions, optionsDispatch] = useReducer(optionsReducer, {});
   const [posts, postsDispatch] = useReducer(postsReducer, postsState);
   const [isOnboarding, setOnboarding] = useState(true);
-  const [trackStopIndex, setTrackStopIndex] = useState(0);
+  //react-virtualized loaded rows and row count.
+  const [itemCount, setItemCount] = useState(0);
+  const [loadedRows, setLoadedRows] = useState(true);
   const {
     filterModal,
     showCreatePostModal,
@@ -411,7 +413,7 @@ const Feed = (props) => {
         ? ""
         : `&filter=${encodeURIComponent(JSON.stringify(filterObj))}`;
     };
-    const limit = 5;
+    const limit = 10;
     const skip = page * limit;
     const baseURL = `/api/posts?limit=${limit}&skip=${skip}`;
     let endpoint = `${baseURL}${objectiveURL()}${filterURL()}`;
@@ -529,42 +531,39 @@ const Feed = (props) => {
     dispatchAction(SET_VALUE, "applyFilters", true);
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const loadNextPage = useCallback(
-    ({ stopIndex }) => {
-      //  console.log(stopIndex, "stopindex", trackStopIndex, "tarckstop")
-      if (stopIndex > trackStopIndex) {
-        if (!isLoading && loadMore) {
-          postsDispatch({ type: NEXT_PAGE });
-        }
-        setTrackStopIndex(stopIndex);
+  const isItemLoaded = useCallback(
+    (index) => {
+      const posts = Object.entries(postsList);
+      if (!!posts[index]) {
+        setLoadedRows(false);
       } else {
-        setTrackStopIndex(stopIndex);
+        setLoadedRows(true);
+      }
+      return !loadMore || !!posts[index];
+    },
+    [postsList, loadMore],
+  );
+
+  const loadNextPage = useCallback(
+    ({ startIndex, stopIndex }) => {
+      if (!isLoading && loadMore && loadedRows) {
+        return new Promise((resolve) =>
+          setTimeout(() => {
+            postsDispatch({ type: NEXT_PAGE });
+            resolve();
+          }, 500),
+        );
+      } else {
+        return Promise.resolve();
       }
     },
-    [trackStopIndex, isLoading, loadMore],
+    [loadedRows, isLoading, loadMore],
   );
-  // const scrollObserver = useCallback(
-  //   (node) => {
-  //     new IntersectionObserver((entries) => {
-  //       entries.forEach(async (entry) => {
-  //         if (entry.intersectionRatio > 0 && !isLoading && loadMore) {
-  //           await postsDispatch({ type: NEXT_PAGE });
-  //         }
-  //       });
-  //     }).observe(node);
-  //   },
-  //   [postsDispatch, loadMore, isLoading],
-  // );
 
-  // useEffect(() => {
-  //   let observer;
-  //   if (bottomBoundaryRef.current) {
-  //     observer = scrollObserver(bottomBoundaryRef.current);
-  //   }
-  //   return () => {
-  //     observer && observer.disconnect();
-  //   };
-  // }, [scrollObserver, bottomBoundaryRef]);
+  useEffect(() => {
+    const posts = Object.entries(postsList);
+    setItemCount(loadMore ? posts.length + 1 : posts.length);
+  }, [loadMore, posts, postsList, setItemCount]);
 
   const postDelete = async (post) => {
     let deleteResponse;
@@ -679,6 +678,8 @@ const Feed = (props) => {
               isNextPageLoading={isLoading}
               loadNextPage={loadNextPage}
               hasNextPage={loadMore}
+              itemCount={itemCount}
+              isItemLoaded={isItemLoaded}
             />
             {status === ERROR_POSTS && (
               <ErrorAlert message={postsError.message} />
